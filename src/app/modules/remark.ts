5125743add770a854c90f4ecf3df59a3f385821a
十界 <sonne@asaki.me>
2017-02-09 20:27:16 +0800

    add remark function for follow lists

--- src/app/modules/remark.ts
+++ src/app/modules/remark.ts
@@ -34,19 +34,17 @@ class RemarkMain extends Remark {
   init () {
     const fullnameEl: HTMLElement = $$('.vcard-fullname')
 
-    if (fullnameEl !== null) {
+    if (fullnameEl !== null && !fullnameEl.innerHTML.includes('ghp-nickname__main')) {
       const name = this.remark[$$('.vcard-username').innerText]
       const username = $$('.vcard-username').innerText
 
       const nickname = document.createElement('span')
-      nickname.className = 'ghp-nickname'
+      nickname.className = 'ghp-nickname__main'
       nickname.innerText = `(${name || '双击修改备注名'})`
 
       nickname.ondblclick = this.editNickname(username, nickname)
 
-      if (!fullnameEl.innerHTML.includes('ghp-nickname')) {
-        $$('.vcard-fullname') && $$('.vcard-fullname').appendChild(nickname)
-      }
+      $$('.vcard-fullname') && $$('.vcard-fullname').appendChild(nickname)
     }
   }
 }
@@ -55,11 +53,41 @@ class RemarkList extends Remark {
   init () {
     if (location.search.includes('tab=follow')) {
       const userNodeList: Array<HTMLElement> = Array.from($('div.d-table-cell.col-9.v-align-top.pr-3 > a'))
+      const listEl = $$('.js-repo-filter.position-relative')
       
-      for (let item of userNodeList) {
-        console.log(item.innerText)
+      listEl.ondblclick = (event) => {
+        const target = <HTMLElement>event.target
+
+        if (/^nickname-\d/.test(target.id)) {
+          const username = this.getUsername(<HTMLElement>target.previousElementSibling)
+
+          this.editNickname(username, target)()
+        }
+      }
+
+      for (let i = 0; i < userNodeList.length; i++) {
+        const item = userNodeList[i]
+
+        if (!item.innerHTML.includes('ghp-nickname__list')) {
+          const username = this.getUsername(item)
+          const fullname = item.innerText.replace(` ${username}`, '')
+
+          const name = this.remark[username]
+
+          const nickname = document.createElement('span')
+          nickname.className = 'ghp-nickname__list'
+          nickname.innerText = `(${name || '双击修改备注名'})`
+          nickname.id = `nickname-${i}`
+
+          item.parentNode.insertBefore(nickname, item.nextSibling)
+        }
       }
     }
+  }
+
+  getUsername (item: HTMLElement) {
+    const nameArray: Array<string> = item.innerText.split(' ')
+    return nameArray[nameArray.length - 1]
   }
 }
 
